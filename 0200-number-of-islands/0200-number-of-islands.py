ec1709b0b676a92ea1cac767ac34ad907ece2b7f
장병우 <63662808+bw-99@users.noreply.github.com>
2025-10-08 15:04:38 +0900

    Time: 254 ms (45.72%), Space: 20.2 MB (57.98%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,38 +1,26 @@
 class Solution:
-    d_list = [[-1, 0], [1, 0], [0, 1], [0, -1]]
+    move = [[-1, 0], [1, 0], [0, 1], [0, -1]]
 
-    def bfs(self, row, col):
-        queue = [(row, col)]
-        while queue:
-            cur_row, cur_col = queue.pop(0)
-            if self.visited[cur_row][cur_col]:
+    def dfs(self, row, col):
+        self.visited[row][col] = True
+
+        for drow, dcol in self.move:
+            nrow, ncol = row+drow, col+dcol
+            if not (0<=nrow<len(self.grid) and 0<=ncol<len(self.grid[0])):
                 continue
+            if not self.visited[nrow][ncol] and self.grid[nrow][ncol] == "1":
+                self.dfs(nrow, ncol)
 
-            self.visited[cur_row][cur_col] = True
-
-            for drow, dcol in self.d_list:
-                next_row, next_col = cur_row + drow, cur_col + dcol
-
-                if not (
-                    (0<=next_row<len(self.grid)) and 
-                    (0<=next_col<len(self.grid[0]))
-                ):
-                    continue
-
-                if not self.visited[next_row][next_col] and self.grid[next_row][next_col] == "1":
-                    queue.append([next_row, next_col])
-
-            
-
-    def numIslands(self, grid: List[List[str]]) -> int:
-        num_row, num_col = len(grid), len(grid[0])
-        self.visited = [[False] * num_col for _ in range(num_row)]
-        self.grid = grid
-        count = 0
         
-        for row in range(num_row):
-            for col in range(num_col):
-                if grid[row][col] == "1" and not self.visited[row][col]:
-                    self.bfs(row, col)
-                    count+=1
+    
+    def numIslands(self, grid: List[List[str]]) -> int:
+        self.grid = grid
+        self.visited = [[False] * len(self.grid[0]) for _ in range(len(self.grid))]
+
+        count = 0
+        for row in range(len(self.grid)):
+            for col in range(len(self.grid[0])):
+                if self.grid[row][col] == "1" and not self.visited[row][col]:
+                    self.dfs(row, col)
+                    count+=1        
         return count
