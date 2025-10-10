3c3fb70bf24c6213e3e8667411a4f3bed5652aa7
장병우 <63662808+bw-99@users.noreply.github.com>
2025-10-10 17:40:26 +0900

    Time: 110 ms (5.13%), Space: 18.1 MB (16.56%) - LeetHub

---
+++ 1993-sum-of-all-subset-xor-totals/1993-sum-of-all-subset-xor-totals.py
@@ -0,0 +1,20 @@
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        self.nums = nums
+        answer = 0
+
+        def dfs(pos, q):
+            nonlocal answer
+            if q:
+                tmp_answer = q[0]
+                for idx in range(1, len(q)):
+                    tmp_answer = tmp_answer ^ q[idx]
+                print(q, tmp_answer)
+                answer += tmp_answer
+            
+            for idx in range(pos, len(self.nums)):
+                dfs(idx+1, q+[self.nums[idx]])
+
+        dfs(0, [])
+        return answer
+        
