16e6f71d5c05bf3e8278d4c97a14f9700237ef1e
장병우 <63662808+bw-99@users.noreply.github.com>
2025-10-12 03:40:14 +0900

    Time: 8 ms (68.41%), Space: 17.9 MB (61.41%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        answer = []
+
+        candidates = sorted(candidates, key=lambda x: -x)
+        
+        def dfs(level: int, residual: int, traces: list[int]):
+            if level == len(candidates):
+                if residual == 0:
+                    result = []
+                    for idx in range(len(traces)):
+                        result.extend([candidates[idx]]*traces[idx])
+                    answer.append(result)
+                return
+            
+            divisor = residual // candidates[level]
+            for quotient in range(divisor+1):
+                dfs(level+1, residual - candidates[level]*quotient, traces + [quotient])
+
+        dfs(0, target, [])
+
+        return answer
